1c65a344b28d505f7c34df721095e8bcaeec6b89
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-12 19:55:00 -0500

    finished styling on login page, and added signin function usingauth from firebase and using the provider, alerting the user with a popup from firabse

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import Head from 'next/head';
 import {Button} from '@material-ui/core';
+import { auth, provider } from "../firebase";
 
 function Login() {
+    const signIn = () => {
+        auth.signInWithPopup(provider).catch(alert);
+    }
+
     return (
         <Container>
             <Head>
@@ -13,7 +18,7 @@ function Login() {
                 <Logo 
                     src='https://cdn.iconscout.com/icon/free/png-256/whatsapp-circle-1868968-1583132.png'
                 />
-                <Button variant='outlined'>Sign in with Google</Button>
+                <Button onClick={signIn} variant='outlined'>Sign in with Google</Button>
             </LoginContainer>
         </Container>
     )
@@ -22,16 +27,26 @@ function Login() {
 export default Login
 
 const Container = styled.div `
-
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background-color: white;
 `;
 
 
 const LoginContainer = styled.div `
-
+    display: flex;
+    flex-direction: column;
+    padding: 100px;
+    align-items: center;
+    background-color: whitesmoke;
+    border-radius: 5px;
+    box-shadow: 0 4px 14px -3px rgba(0, 0, 0, 0.7)
 `;
 
 
 const Logo = styled.img `
     height: 200px;
     width: 200px;
+    margin-bottom: 50px;
 `;
