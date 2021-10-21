637a71a006f20e4bb465963e94e7be5d606cdb44
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-20 20:55:57 -0500

    created server side rendering function to go into doc where id matches wildcard id and preps the messages on the server side, along with recuperating the time stamp

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Head from 'next/head';
 import Sidebar from '../../components/Sidebar';
 import ChatScreen from '../../components/ChatScreen';
+import { db } from '../../firebase';
 
 function Chat({}) {
     return (
@@ -17,7 +18,22 @@ function Chat({}) {
     )
 }
 
-export default Chat
+export default Chat;
+
+//server pre renders page and throws it out once it is requested
+export async function getServerSideProps(context) {
+    //goes into document of chat where id matches wildcard id
+    const ref = db.collection('chats').doc(context.query.id);
+    //prep the messages on the server side
+    const messageRes = await ref.collection('messages').orderBy('timestamp', 'asc').get();
+    const messages = messageRes.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })).map((messages) => ({
+        ...messages,
+        timestamp: messages.timestamp.toDate().getTime()
+    }));
+}
 
 
 const Container = styled.div `
