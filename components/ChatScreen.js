31bd2d0dc2bcf45387c5d4278b670c53fe33a115
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-26 20:19:57 -0500

    added styling to header and to header information

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -37,8 +37,30 @@ export default ChatScreen
 
 const Container = styled.div ``;
 
-const Header = styled.div ``;
+const Header = styled.div `
+    position: sticky;
+    background-color: white;
+    z-index: 100;
+    top: 0;
+    display: flex;
+    padding: 11px;
+    height: 80px;
+    align-items: center;
+    border-bottom: 1px solid whitesmoke;
+`;
 
-const HeaderInformation = styled.div ``;
+const HeaderInformation = styled.div `
+    margin-left: 15px;
+    flex: 1;
+
+    > h3 {
+        margin-bottom: 3px;
+    }
+
+    > p {
+        font-size: 14px;
+        color: gray;
+    }
+`;
 
 const HeaderIcons = styled.div ``;
