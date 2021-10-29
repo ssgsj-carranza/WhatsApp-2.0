f2855c46ecf43f9fe9a3e54bcdc5629f5b8d6898
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-28 20:56:47 -0500

    began building styled element components for message element and expanded to change depending on sender or reciever

--- components/Message.js
+++ components/Message.js
@@ -1,6 +1,13 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import {auth} from '../firebase';
 
 function Message({user, message}) {
+    const [userLoggedIn] = useAuthState(auth);
+
+    //Determening sender or reciever
+
+
     return (
         <Container>
             <p>{message.message}</p>
@@ -12,3 +19,24 @@ export default Message
 
 
 const Container = styled.div ``;
+
+const MessageElement = styled.p `
+    width: fit-content;
+    padding: 15px;
+    border-radius: 8px;
+    margin: 10;
+    min-width: 60px;
+    padding-bottom: 26px;
+    position: relative;
+    text-align: center;
+`;
+
+const Sender = styled(MessageElement)`
+    margin-left: auto;
+    background-color: #dcf8c6;
+`;
+
+const Reciever = styled(MessageElement)`
+    background-color: whitesmoke;
+    text-align: left;
+`;
